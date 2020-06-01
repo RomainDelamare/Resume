b86a47129722fb37fd4c9f51feab7cd1f6be628c
Romain Delamare <romain.delamare.fr@gmail.com>
2020-06-01 12:53:14 +0200

    CV in progress

---
+++ src/PersonalWebsite/Components/Timeline/TimelineViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Components.Timeline
+{
+    public class TimelineViewModel
+    {
+        public EventViewModel[] Events { get; set; }
+    }
+
+    public class EventViewModel
+    {
+        public string Name { get; set; }
+        public string Supplement { get; set; }
+        public IconInfoViewModel[] IconInfos { get; set; }
+        public string Description { get; set; }
+        public int From { get; set; }
+        public int? To { get; set; }
+    }
+
+    public class IconInfoViewModel
+    {
+        public string Label { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ src/PersonalWebsite/Pages/CV/CVViewModel.cs
@@ -0,0 +1,41 @@
+﻿using PersonalWebsite.Components.CV.Skill;
+using PersonalWebsite.Components.Timeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Pages.CV
+{
+    public class CVViewModel
+    {
+        public string Name { get; set; }
+        public string JobPosition { get; set; }
+        public string Picture { get; set; }
+        public string Aboutme { get; set; }
+        public ContactViewModel[] Contacts { get; set; }
+        public SkillViewModel[] Skills { get; set; }
+        public TimelineViewModel Experience { get; set; }
+        public TimelineViewModel Education { get; set; }
+        public HobbiesViewModel[] Hobbies { get; set; }
+    }
+
+    public class ContactViewModel
+    {
+        public string Link { get; set; }
+        public string Label { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class SkillViewModel
+    {
+        public string Label { get; set; }
+        public int Percentage { get; set; }
+    }
+
+    public class HobbiesViewModel
+    {
+        public string Label { get; set; }
+        public string Icon { get; set; }
+    }
+}
