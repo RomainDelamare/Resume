3d0e810a88732dea58982aa4807d1c6350280427
Romain Delamare <romain.delamare.fr@gmail.com>
2020-06-01 19:25:20 +0200

    add translation

--- src/PersonalWebsite/Program.cs
+++ src/PersonalWebsite/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.LocalStorage;
 
 namespace PersonalWebsite
 {
@@ -17,7 +18,9 @@ namespace PersonalWebsite
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services
+                .AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
