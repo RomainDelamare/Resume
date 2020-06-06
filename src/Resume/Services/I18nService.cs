03805037a825761705f0f944e89f701ef4c85774
Romain Delamare <romain.delamare.fr@gmail.com>
2020-06-06 12:11:33 +0200

    Refacto

---
+++ src/Resume/Services/I18nService.cs
@@ -0,0 +1,46 @@
+﻿using Blazored.LocalStorage;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Services
+{
+    public interface II18nService
+    {
+        Task<string> GetLang();
+        Task SetLang(string lang);
+    }
+
+    public class I18nService : II18nService
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        private readonly string LANG_STORAGE_KEY = "lang";
+        private readonly string[] ACCEPTED_LANG = new string[] { "fr", "en" };
+        private readonly string FALLBACK_LANG = "en";
+
+        public I18nService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task<string> GetLang()
+        {
+            var lang = await _localStorage.GetItemAsync<string>(LANG_STORAGE_KEY);
+            if (ACCEPTED_LANG.Contains(lang))
+                return lang;
+
+            lang = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+            if (ACCEPTED_LANG.Contains(lang))
+                return lang;
+
+            return FALLBACK_LANG;
+        }
+
+        public async Task SetLang(string lang)
+        {
+            await _localStorage.SetItemAsync(LANG_STORAGE_KEY, lang);
+        }
+    }
+}
---
+++ src/Resume/Translation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume
+{
+    public class Translation
+    {
+        public string ABOUT_ME { get; set; }
+        public string SKILLS { get; set; }
+        public string EXPERIENCES { get; set; }
+        public string EDUCATIONS_AND_DIPLOMAS { get; set; }
+        public string HOBBIES { get; set; }
+        public string DOWNLOAD_RESUME { get; set; }
+        public string SWITCH_LANGUAGE { get; set; }
+    }
+}
