377ff8d148c4215723f9db7b3a74d91b8cca3da7
Romain Delamare <romain.delamare.fr@gmail.com>
2020-06-10 22:58:41 +0200

    add blur on click

---
+++ src/Resume/wwwroot/scripts/BlurManagement.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('click', function (e) {
+    console.log(document.activeElement.toString());
+    if (document.activeElement.toString() == '[object HTMLButtonElement]' || document.activeElement.toString() == '[object HTMLInputElement]') {
+        document.activeElement.blur();
+    }
+});
